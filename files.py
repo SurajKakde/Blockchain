7ba456624de3658ce2a34f1c904b470062bdcb29
SurajKakde <suraj.lats@gmail.com>
2019-12-31 19:38:59 -0500

    working with files - Module 7

--- files.py
+++ files.py
@@ -1,15 +1,16 @@
-f = open('demo.txt', mode='r')
-# # f.write('Add this content!\n')
-# fl = f.readlines()
-# f.close()
-#
-# for line in fl:
-#     print(line[:-1])
+with open('demo.txt', mode='r') as f:
+    # # f.write('Add this content!\n')
+    # fl = f.readlines()
+    # f.close()
+    #
+    # for line in fl:
+    #     print(line[:-1])
 
-line = f.readline()
-while line:
-    print(line)
     line = f.readline()
+    while line:
+        print(line)
+        line = f.readline()
 
-# print(f.readline())
-f.close()
+    # print(f.readline())
+
+print('Done!')
