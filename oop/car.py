9d7ce2b2de977ee57344db3dde658f9fa10bfa8d
SurajKakde <suraj.lats@gmail.com>
2020-01-05 22:54:56 -0500

    Module 9 - Class and objects

--- oop/car.py
+++ oop/car.py
@@ -1,6 +1,19 @@
 class Car:
-    top_speed = 100
-    warnings = []
+    # top_speed = 100
+    # warnings = []
+    def __init__(self, starting_top_speed=100):
+        self.top_speed = starting_top_speed
+        self.__warnings = []
+
+    def add_warnings(self, warning_text):
+        self.__warnings.append(warning_text)
+
+    def get_warnings(self):
+        return self.__warnings
+
+    def __repr__(self):
+        print('Printing ...')
+        return 'top_speed: {}, warning: {}'.format(self.top_speed, len(self.__warnings))
 
     def drive(self):
         print('I am driving but certainly not faster than {}'.format(self.top_speed))
@@ -8,13 +21,16 @@ class Car:
 car1 = Car()
 car1.drive()
 
-Car.top_speed = 200
-car1.warnings.append('New year!')
+# Car.top_speed = 200
+car1.add_warnings('New warning')
+# car1.__warnings.append('New year!')
+# print(car1.__dict__)
+print(car1)
 
-car2 = Car()
+car2 = Car(200)
 car2.drive()
-print(car2.warnings)
+print(car2.get_warnings())
 
-car3 = Car()
+car3 = Car(250)
 car3.drive()
-print(car3.warnings)
+print(car3.get_warnings())
