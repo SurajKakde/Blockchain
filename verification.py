074d4eac3969b447c93cd398a3ba85b4479a7935
SurajKakde <suraj.lats@gmail.com>
2020-01-10 22:05:40 -0500

    Module 9 - Class and objects - using decorators for verification class

--- verification.py
+++ verification.py
@@ -2,30 +2,34 @@ from hash_util import hash_string_256, hash_block
 
 
 class Verification:
-    def valid_proof(self, transactions, last_hash, proof):
+    @staticmethod
+    def valid_proof(transactions, last_hash, proof):
         guess = (str([tx.to_ordered_dict() for tx in transactions]) + str(last_hash) + str(proof)).encode()
         guess_hash = hash_string_256(guess)
         print(guess_hash)
         return guess_hash[0:2] == '00'
 
-    def verify_blockchain(self, blockchain):
+    @classmethod
+    def verify_blockchain(cls, blockchain):
         """ verify the block chain and return True if valid """
         for (index, block) in enumerate(blockchain):
             if index == 0:
                 continue
             elif block.previous_hash != hash_block(blockchain[index - 1]):
                 return False
-            if not self.valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
+            if not cls.valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
                 print('Proof of work is invalid!')
                 return False
         return True
 
-    def verify_transaction(self, transaction, get_balance):
+    @staticmethod
+    def verify_transaction(transaction, get_balance):
         sender_balance = get_balance()
         return sender_balance >= transaction.amount
 
-    def validate_transactions(self, open_transactions, get_balance):
+    @classmethod
+    def validate_transactions(cls, open_transactions, get_balance):
         """Verifies all open transaction"""
-        return all([self.verify_transaction(tx, get_balance) for tx in open_transactions])
+        return all([cls.verify_transaction(tx, get_balance) for tx in open_transactions])
 
 
