9f94c64d849051c73d0957e216260e6aa9b9b8f5
SurajKakde <suraj.lats@gmail.com>
2020-02-05 23:49:35 -0500

    module 12 - broad casting block and transaction to other node. conflict is in resetting open transaction

--- blockchain.py
+++ blockchain.py
@@ -180,8 +180,8 @@ class Blockchain:
         #     'recipient': recipient,
         #     'amount': amount
         # }
-        if self.public_key is None:
-            return False
+        # if self.public_key is None:
+        #     return False
         transaction = Transaction(sender, recipient, signature, amount)
         if Verification.verify_transaction(transaction, self.get_balance):
             self.__open_transactions.append(transaction)
